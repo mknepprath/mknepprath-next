ba7b6a4461ed1aa00149f8c2c373e4639d55b2f7
Michael Knepprath <mknepprath@gmail.com>
2018-12-23 10:12:38 -0600

    Add lang='en'

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+// `_document.js` is only rendered on the server side and not on the client side.
+// Event handlers like onClick can't be added to this file.
+
+// See documentation: https://github.com/zeit/next.js#custom-document
+
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      // This is the reason this file was added - to set the `lang` attribute.
+      <html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
