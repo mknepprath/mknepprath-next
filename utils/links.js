f2d302773e030f6d1f96982d096bb26ac43e86d1
m0k013z <michael.knepprath@walmartlabs.com>
2019-05-20 21:42:30 -0500

    Update projects, make it Prettierrrrrrr

--- utils/links.js
+++ utils/links.js
@@ -1,81 +1,79 @@
 /** @type {Array<{ href: string, key: string, label: string }>} */
 export const footerLinks = [
   // { href: 'https://twitter.com/mknepprath', key: 'twitter', label: 'The Bird Site' },
-  { href: 'mailto:michael@mknepprath.com', key: 'contact', label: 'Contact Me' }
-]
+  { href: "mailto:michael@mknepprath.com", key: "contact", label: "Contact Me" }
+];
 
 /** @type {Array<{ href: string, key: string, label: string }>} */
 export const navLinks = [
-  { href: 'https://github.com/mknepprath', key: 'code', label: 'Code' },
-  { href: 'https://dribbble.com/mknepprath', key: 'illustration', label: 'Illustration' },
-  { href: 'https://medium.com/@mknepprath', key: 'writing', label: 'Writing' },
-  { href: '/about', key: 'about', label: 'About' }
-]
+  { href: "https://github.com/mknepprath", key: "code", label: "Code" },
+  {
+    href: "https://dribbble.com/mknepprath",
+    key: "illustration",
+    label: "Illustration"
+  },
+  { href: "https://medium.com/@mknepprath", key: "writing", label: "Writing" },
+  { href: "/about", key: "about", label: "About" }
+];
 
 /** @type {Array<{ description: string, href: string, label: string, url: string }>} */
 export const projectLinks = [
   {
-    description: 'Movie tracking, simplified',
-    href: 'http://moviestarapp.com/',
-    label: '★ Movie Star',
-    url: '/static/movie-star.png'
+    description: "A Twitter text adventure",
+    href: "https://twitter.com/familiarlilt",
+    label: "Lilt",
+    url: "/static/lilt.png"
   },
   {
-    description: 'A Twitter text adventure',
-    href: 'https://twitter.com/familiarlilt',
-    label: 'Lilt',
-    url: '/static/lilt.png'
+    description: "A prompt a day, every day",
+    href: "https://twitter.com/designprompts",
+    label: "Design Prompts",
+    url: "/static/design-prompts.png"
   },
   {
-    description: 'A prompt a day, every day',
-    href: 'https://twitter.com/designprompts',
-    label: 'Design Prompts',
-    url: '/static/design-prompts.png'
+    description: "Illustration series",
+    href:
+      "https://dribbble.com/mknepprath/buckets/257652-Design-Prompts-Series",
+    label: "Dribbble Series",
+    url: "/static/dribbble-series.png"
   },
   {
-    description: 'Illustration series',
-    href: 'https://dribbble.com/mknepprath/buckets/257652-Design-Prompts-Series',
-    label: 'Dribbble Series',
-    url: '/static/dribbble-series.png'
+    description: "@horse_ebooks Twitter bot",
+    href: "https://twitter.com/robot_mk",
+    label: "Robot MK",
+    url: "/static/robot-mk.png"
   },
   {
-    description: '@horse_ebooks Twitter bot',
-    href: 'https://twitter.com/robot_mk',
-    label: 'Robot MK',
-    url:'/static/robot-mk.png'
+    description: "A thread of Cinema 4D renders",
+    href: "https://twitter.com/mknepprath/status/959812218119512065",
+    label: "Cinema 4D Animations"
   },
   {
-    description: 'A thread of Cinema 4D renders',
-    href: 'https://twitter.com/mknepprath/status/959812218119512065',
-    label: 'Cinema 4D Animations'
-
+    description: "Chrome extension",
+    href:
+      "https://chrome.google.com/webstore/detail/patched-tomatoes/daceaaccoiggipikdodkejceenipddop",
+    label: "Patched Tomatoes",
+    url: "/static/patched-tomatoes.png"
   },
   {
-    description: 'Chrome extension',
-    href: 'https://chrome.google.com/webstore/detail/patched-tomatoes/daceaaccoiggipikdodkejceenipddop',
-    label: 'Patched Tomatoes',
-    url: '/static/patched-tomatoes.png'
+    description: "React RPG framework",
+    href: "https://github.com/mknepprath/open-world",
+    label: "Open World"
   },
   {
-    description: 'React RPG framework',
-    href: 'https://github.com/mknepprath/open-world',
-    label: 'Open World'
+    description: "Sequential art",
+    href: "https://twitter.com/mknepprath/status/1060284635182178305",
+    label: "Comics"
   },
   {
-    description: 'Sequential art',
-    href: 'https://twitter.com/mknepprath/status/1060284635182178305',
-    label: 'Comics'
-
+    description: "Battle your friends on Twitter",
+    href: "https://twitter.com/bout_bot",
+    label: "Bout"
   },
   {
-    description: 'Battle your friends on Twitter',
-    href: 'https://twitter.com/bout_bot',
-    label: 'Bout'
-  },
-  {
-    description: 'College projects',
-    href: 'https://vimeo.com/mknepprath',
-    label: 'Film & Animation',
-    url: '/static/film.png'
+    description: "College projects",
+    href: "https://vimeo.com/mknepprath",
+    label: "Film & Animation",
+    url: "/static/film.png"
   }
-]
+];
