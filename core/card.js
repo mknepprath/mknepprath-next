781833af81885883b307cf9b6f5ecba360c97322
m0k013z <michael.knepprath@walmartlabs.com>
2019-07-18 22:25:27 -0400

    Remove  Link from projects

--- core/card.js
+++ core/card.js
@@ -4,15 +4,21 @@ import Link from "next/link";
 import styles from "./card.css";
 
 export default ({ description, href, imgSrc, label }) => (
-  <Link href={href} key={label}>
-    <a className={styles.card} rel="noopener" target="_blank">
-      {imgSrc ? <img alt={label} className={styles.img} src={imgSrc} /> : null}
-      <div>
-        <h3>
-          {label} <span className={styles.arrow}>&rarr;</span>
-        </h3>
-        <p>{description}</p>
-      </div>
-    </a>
-  </Link>
+  // <Link href={href} key={label}>
+  <a
+    className={styles.card}
+    href={href}
+    key={label}
+    rel="noopener"
+    target="_blank"
+  >
+    {imgSrc ? <img alt={label} className={styles.img} src={imgSrc} /> : null}
+    <div>
+      <h3>
+        {label} <span className={styles.arrow}>&rarr;</span>
+      </h3>
+      <p>{description}</p>
+    </div>
+  </a>
+  // </Link>
 );
