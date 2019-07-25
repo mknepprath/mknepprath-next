c3babb6c8d7e6b17231d622e700c3d7545370383
m0k013z <michael.knepprath@walmartlabs.com>
2019-07-25 16:48:56 -0400

    Add more old posts

---
+++ pages/writing/archive/buffer.js
@@ -0,0 +1,74 @@
+import Head from "next/head";
+
+import Page from "core/page";
+
+export default () => (
+  <Page>
+    <Head>
+      <title key="title">Become More Effective On Twitter with Buffer</title>
+    </Head>
+
+    <div className={"blog-container container"}>
+      <h1>Become More Effective On Twitter with Buffer</h1>
+
+      <p>
+        I ran into a problem recently. I would read a bunch of posts and share
+        them all on Twitter right away. My followers would get inundated with
+        updates, and usually not at a time that was ideal for them. How was I to
+        continue sharing all of my discoveries without losing all of my
+        followers?
+      </p>
+      <p>
+        <a href="http://bufferapp.com/r/507fd">Buffer</a> had the answer.
+      </p>
+      <p>
+        Buffer allows you to create a schedule for your tweets, spreading them
+        out so that your posts are consistent, but not overwhelming. Here’s how
+        to make the most of Buffer:
+      </p>
+      <ol>
+        <li>
+          <strong>
+            Install the{" "}
+            <a href="http://bufferapp.com/extras">Buffer extensions and apps</a>
+            .
+          </strong>{" "}
+          You need these if you want to take full advantage of Buffer. I use the
+          Chrome extension, which puts Buffer at the top of the browser at all
+          times, and also integrates a Buffer button into Twitter, Facebook,
+          Reddit, and many more websites.
+        </li>
+        <li>
+          <strong>Keep your Buffer queue full.</strong> Whenever you find
+          anything you want to tweet, click Buffer. A window will open where you
+          can tweak the post that is automatically generated. Once you’ve
+          perfected the post, click Add to Buffer.
+        </li>
+        <li>
+          <strong>Reorder tweets & tweak settings.</strong> When you visit
+          Buffer’s website, you will be taken to your dashboard. Here you are
+          able to see all of your tweets and what times they are scheduled to go
+          live. You’re able to switch the order, edit the posts, and edit the
+          default schedule. As you use Buffer more, you’ll begin to discover
+          which times are the most effective and which ones may need to be
+          changed. The default schedule was calculated to be the most ideal for
+          everyone, not necessarily for your audience.
+        </li>
+      </ol>
+      <p>
+        If you plan on using this tool, I recommend that you{" "}
+        <a href="http://bufferapp.com/r/507fd">follow this link</a> and sign up.
+        This will give us both a referral bonus of one extra space in our
+        queues.
+      </p>
+      <p>
+        What tools do you currently use to manage your tweets, if any? I’d love
+        to hear about them and try them out!
+      </p>
+
+      <p className={"blog-time"}>
+        <time dateTime="2012-07-10">July 10, 2012</time>
+      </p>
+    </div>
+  </Page>
+);
---
+++ pages/writing/archive/pinterest.js
@@ -0,0 +1,91 @@
+import Head from "next/head";
+
+import Page from "core/page";
+
+export default () => (
+  <Page>
+    <Head>
+      <title key="title">Harness the Power of Pinterest</title>
+    </Head>
+
+    <div className={"blog-container container"}>
+      <h1>Harness the Power of Pinterest</h1>
+
+      <p>
+        Unless you’ve been hiding under a rock, you probably heard about
+        Pinterest’s recent and rapid rise to fame. Pinterest is a “virtual
+        pinboard” where one is able to post images from anywhere on the web and
+        share them with their friends. It wasn’t originally targeted towards any
+        particular audience, but the user base is currently{" "}
+        <a href="http://mashable.com/2012/07/04/men-women-social-media/">
+          predominantly women
+        </a>
+        .
+      </p>
+      <p>
+        Many businesses are just now discovering that there is value in
+        participating in social media, but where does Pinterest fit into this
+        mix?
+      </p>
+      <p>
+        Pinterest, a network composed of found graphics and photos, functions
+        differently from other social media sites you may be used to. Here are a
+        few tips to help you be more successful on this new platform.
+      </p>
+      <ol>
+        <li>
+          <strong>Tailor your images for the audience. </strong>As mentioned
+          earlier, Pinterest is primarily women. As such, it’s not surprising
+          that some of the top brands on the site are related to wedding
+          planning, parenting, chocolate, and clothing. You (or your business)
+          don’t necessarily have to fall into these categories, but be sure that
+          you are targeting the correct audience.
+        </li>
+        <li>
+          <strong>Create shareable images. </strong>The main size at which your
+          images will be viewed is relatively small, a measly 192 pixels across.
+          Therefore, if you are planning on including words, make them large
+          enough to be legible at that width. There will be a much greater
+          chance of people reading what you are attempting to share with them.
+        </li>
+        <li>
+          <strong>Be aware of the trends.</strong> Speaking of including words
+          in your pins, doing just that is highly recommended. Inspirational
+          quotes go viral frequently, as many users like and relate to them.
+          Other trends include food, hair, shoes, dresses, and design.
+        </li>
+        <li>
+          <strong>Create a gallery on your website.</strong> This mainly applies
+          to businesses, and is also where the marketing value of Pinterest
+          lies. When a person pins an interesting or beautiful image from a
+          website, their friends will start to share it with each other. Then
+          their friends share it with their friends. If that doesn’t already
+          sound amazing, here’s the clincher. Each of these pins is a link back
+          to the website they were originally pinned from. Yours.
+        </li>
+        <li>
+          <strong>Consistently share new pins.</strong> The most important step
+          is also the most difficult. Be consistent. Add pins on a regular basis
+          so people have a reason to keep coming back. If your page grows
+          stagnant, they will stop checking.
+        </li>
+      </ol>
+      <p>
+        To see some of these tips in action, here’s{" "}
+        <a href="http://www.socialmediaexaminer.com/how-alaskan-mom-brings-millions-to-her-carpentry-blog/">
+          a cool case study
+        </a>{" "}
+        about an Alaskan mom who draws millions of people to her carpentry blog
+        through Pinterest.
+      </p>
+      <p>
+        What do you think? Do you have any tips through your experience on
+        Pinterest?
+      </p>
+
+      <p className={"blog-time"}>
+        <time dateTime="2012-07-06">July 6, 2012</time>
+      </p>
+    </div>
+  </Page>
+);
