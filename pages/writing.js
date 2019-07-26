33141864cc048eba67c826e0c68af09a5b057966
m0k013z <michael.knepprath@walmartlabs.com>
2019-07-26 07:14:32 -0400

    Add query param to blog

--- pages/writing.js
+++ pages/writing.js
@@ -1,34 +1,47 @@
 import Head from "next/head";
 import Link from "next/link";
+import { withRouter } from "next/router";
 import parse from "date-fns/parse";
 
 import Page from "core/page";
 
 import { posts } from "../posts.json";
 
-export default () => (
-  <Page>
-    <Head>
-      <title key="title">Michael Knepprath, Occasional Writer</title>
-    </Head>
+class Writing extends React.Component {
+  render() {
+    const {
+      router: { query }
+    } = this.props;
 
-    <div className={"blog-container container"}>
-      <h1>Writing</h1>
-      <ul>
-        {posts
-          .filter(post => post.published)
-          // The `sort` method can be conveniently used with function expressions:
-          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-          .sort((a, b) => parse(b.date) - parse(a.date))
-          .map(post => (
-            <li key={post.id}>
-              <Link href={`/writing/${post.id}`}>
-                <a>{post.title}</a>
-              </Link>{" "}
-              {post.date}
-            </li>
-          ))}
-      </ul>
-    </div>
-  </Page>
-);
+    return (
+      <Page>
+        <Head>
+          <title key="title">Michael Knepprath, Occasional Writer</title>
+        </Head>
+
+        <div className={"blog-container container"}>
+          <h1>Writing</h1>
+          <ul>
+            {posts
+              // Including this query param will display all posts.
+              // https://mknepprath.com/writing?all=true
+              .filter(post => post.published || query.all)
+              // The `sort` method can be conveniently used with function expressions:
+              // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+              .sort((a, b) => parse(b.date) - parse(a.date))
+              .map(post => (
+                <li key={post.id}>
+                  <Link href={`/writing/${post.id}`}>
+                    <a>{post.title}</a>
+                  </Link>{" "}
+                  {post.date}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </Page>
+    );
+  }
+}
+
+export default withRouter(Writing);
