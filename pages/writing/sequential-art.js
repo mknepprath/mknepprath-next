f2ca82fb554ef0dcb876af1138b2e3f5e5fccc61
m0k013z <mknepprath@gmail.com>
2019-12-12 13:15:43 -0500

    Add images and post draft

---
+++ pages/writing/sequential-art.js
@@ -0,0 +1,224 @@
+import BlogPage from "core/blog-page";
+
+export default () => (
+  <BlogPage
+    dateTime="2019-12-12"
+    description="A comic retrospective."
+    ogImage="/static/comics-05.jpeg"
+    title="Sequential Art™"
+  >
+    <header>
+      <h1>Sequential Art™</h1>
+    </header>
+    <p>
+      Comics, both web and traditional, have been a fascination of mine for some
+      time. I wasn't brave enough to try my hand at creating them until I made a
+      discovery during{" "}
+      <a href="https://twitter.com/search?q=from%3A%40mknepprath%20%23inktober">
+        #inktober
+      </a>{" "}
+      2017 - the best way to get over drawing timidity is to use bold drawing
+      tools. I started with pencil, transitioned to pen, and eventually cracked
+      open my son's marker set. My resulting illustrations were as brazen as the
+      drawing tools I was choosing to use.
+    </p>
+    <img
+      alt="A pumpkin illustration"
+      className="blog-image"
+      src="/static/comics-01.jpeg"
+    />
+    <p></p>
+    <img
+      alt="A canoe illustration"
+      className="blog-image"
+      src="/static/comics-02.jpeg"
+    />
+    <p>
+      Last year, I decided to use this technique to draw some panels. It was
+      rough, but I had to start somewhere.
+    </p>
+    <img
+      alt="Make comics. Who has time for that!"
+      className="blog-image"
+      src="/static/comics-03.jpeg"
+    />
+    <p>
+      I posted my first comic as a reply to{" "}
+      <a href="https://twitter.com/JamesInks/status/1021770737877692422">
+        @JamesInks
+      </a>
+      , a talented comic artist, as a gag. I believe I was planning on replying
+      to a bunch of tweets with comics, but I only ended up posting two this
+      way.
+    </p>
+    <img
+      alt="I love pie!!!"
+      className="blog-image"
+      src="/static/comics-04.jpeg"
+    />
+    <p>
+      The second was in reply to{" "}
+      <a href="https://twitter.com/JamesInks/status/1023671020560035842">
+        a tweet about pie
+      </a>
+      .
+    </p>
+    <img
+      alt="Sequential art"
+      className="blog-image"
+      src="/static/comics-05.jpeg"
+    />
+    <p>
+      After this, I started{" "}
+      <a href="https://twitter.com/mknepprath/status/1024083111091597312">
+        a thread
+      </a>{" "}
+      that now includes 19 short comics. This first one,{" "}
+      <strong>Sequential Art™</strong> was meant to be a light-hearted jab at
+      the self-seriousness of some comic artists.
+    </p>
+    <img
+      alt="Sequential stress"
+      className="blog-image"
+      src="/static/comics-06.jpeg"
+    />
+    <p>
+      I tried my hand at social commentary for my 4th comic,{" "}
+      <strong>Sequential Stress™</strong>. These panels ended up being featured
+      in Owen Williams' (<a href="http://twitter.com/ow">@ow</a>) article
+      entitled, "
+      <a href="https://char.gd/blog/2019/mobile-notifications-are-broken-its-time-to-fix-them">
+        Notifications are broken. It's time to fix them.
+      </a>
+      "
+    </p>
+    <img
+      alt="Demon bug!!"
+      className="blog-image"
+      src="/static/comics-07.jpeg"
+    />
+    <p>
+      At this point, I was attempting to create at least one comic per day, so
+      the subject matter turned trivial things I was dealing with. Like bugs.
+    </p>
+    <img
+      alt="Apple Watch activity rings"
+      className="blog-image"
+      src="/static/comics-08.jpeg"
+    />
+    <p>And exercise.</p>
+    <img
+      alt="I will say nothing"
+      className="blog-image"
+      src="/static/comics-09.jpeg"
+    />
+    <p>
+      There was a third panel here that turned out so bad, I cut it off. I
+      enjoyed this two-panel reaction shot to post it despite that.
+    </p>
+    <img
+      alt="Flat La Croix"
+      className="blog-image"
+      src="/static/comics-10.jpeg"
+    />
+    <p>
+      Most of my comics don't warrant a title. Perhaps this one didn't, either,
+      but I decided to include it for some additional context.
+    </p>
+    <img
+      alt="Silly faces"
+      className="blog-image"
+      src="/static/comics-11.jpeg"
+    />
+    <p>My son and I take pictures of our silly faces sometimes.</p>
+    <img alt="Hair study" className="blog-image" src="/static/comics-12.jpeg" />
+    <p>
+      My hairstyle was in transition during this time. I did a few quick studies
+      to nail how I wanted to portray it on the page.
+    </p>
+    <img alt="Duck" className="blog-image" src="/static/comics-13.jpeg" />
+    <p>
+      While simple, this is the first one where I played with the perspective.
+      Perhaps a sequel is warranted, "honk."
+    </p>
+    <img
+      alt="Butterflies"
+      className="blog-image"
+      src="/static/comics-14.jpeg"
+    />
+    <p>
+      The goal of this study was to illustrate butterflies at various levels of
+      resolution in interesting ways. Note that they become hearts as they fade
+      into the distance.
+    </p>
+    <img
+      alt="Hair problems"
+      className="blog-image"
+      src="/static/comics-15.jpeg"
+    />
+    <p>Again, hair transition issues.</p>
+    <img
+      alt="Disappearing can"
+      className="blog-image"
+      src="/static/comics-16.jpeg"
+    />
+    <p>
+      Here, I attempted to portray a simple thing in comic form that couldn't
+      happen in real life.
+    </p>
+    <img
+      alt="Robot vacuum"
+      className="blog-image"
+      src="/static/comics-17.jpeg"
+    />
+    <p>Using line thickness and texture, I created a biopic of our RoboVac.</p>
+    <img alt="RAAWRRRRR" className="blog-image" src="/static/comics-18.jpeg" />
+    <p>
+      After a brief hiatus, I came back to comics with a fresh perspective on my
+      previous panels. I had become to careful, and it was time to loosen up a
+      bit. This cat comic could be see more feral version of the original "MAKE
+      COMICS" strip.
+    </p>
+    <img
+      alt="Candy witch"
+      className="blog-image"
+      src="/static/comics-19.jpeg"
+    />
+    <p>
+      These few comics were illustrated around Halloween 2018. I drew the candy
+      witch, a witch who trades a toy for candy.
+    </p>
+    <img
+      alt="Angsty pumpkin"
+      className="blog-image"
+      src="/static/comics-20.jpeg"
+    />
+    <p>I also found the idea of an angsty pumpkin hilarious.</p>
+    <img
+      alt="Contemplative fisherman"
+      className="blog-image"
+      src="/static/comics-21.jpeg"
+    />
+    <p>
+      This started out as a hand study, but resulted in one of my favorite
+      strips - more realistic with multiple perspectives. There is a nostalgia
+      to it, as well. I haven't gone fishing in years, if not decades.
+    </p>
+    <img
+      alt="Dragon costume"
+      className="blog-image"
+      src="/static/comics-22.jpeg"
+    />
+    <p>
+      Back to the Halloween theme, I thought it would be fun to illustrate a
+      dragon - with a twist. It was my son's costume all along.
+    </p>
+    <img
+      alt="Coffee high"
+      className="blog-image"
+      src="/static/comics-23.jpeg"
+    />
+    <p>Finally, a reaction shot to disappearing a mug of coffee.</p>
+    <p>I had a lot of fun with these, and I hope to do more.</p>
+  </BlogPage>
+);
