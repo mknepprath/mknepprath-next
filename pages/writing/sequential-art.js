5c65ddc0627c0cd5244ba84e6dfd64d742f9dd73
m0k013z <mknepprath@gmail.com>
2020-01-07 16:34:28 -0500

    Update images in sequential art post

--- pages/writing/sequential-art.js
+++ pages/writing/sequential-art.js
@@ -34,16 +34,8 @@ export default () => (
       src="/assets/comics-02.jpeg"
     />
     <p>
-      Last year, I decided to use this technique to draw some panels. It was
-      rough, but I had to start somewhere.
-    </p>
-    <img
-      alt="Make comics. Who has time for that!"
-      className="blog-image"
-      src="/assets/comics-03.jpeg"
-    />
-    <p>
-      I posted my first comic as a reply to{" "}
+      Last year, I decided to use this technique to draw some panels. I posted
+      my first comic as a reply to{" "}
       <a href="https://twitter.com/JamesInks/status/1021770737877692422">
         @JamesInks
       </a>
@@ -52,9 +44,9 @@ export default () => (
       way.
     </p>
     <img
-      alt="I love pie!!!"
+      alt="Make comics. Who has time for that!"
       className="blog-image"
-      src="/assets/comics-04.jpeg"
+      src="/assets/comics-03.jpeg"
     />
     <p>
       The second was in reply to{" "}
@@ -64,9 +56,9 @@ export default () => (
       .
     </p>
     <img
-      alt="Sequential art"
+      alt="I love pie!!!"
       className="blog-image"
-      src="/assets/comics-05.jpeg"
+      src="/assets/comics-04.jpeg"
     />
     <p>
       After this, I started{" "}
@@ -78,9 +70,9 @@ export default () => (
       the self-seriousness of some comic artists.
     </p>
     <img
-      alt="Sequential stress"
+      alt="Sequential art"
       className="blog-image"
-      src="/assets/comics-06.jpeg"
+      src="/assets/comics-05.jpeg"
     />
     <p>
       I tried my hand at social commentary for my 4th comic,{" "}
@@ -93,78 +85,91 @@ export default () => (
       "
     </p>
     <img
-      alt="Demon bug!!"
+      alt="Sequential stress"
       className="blog-image"
-      src="/assets/comics-07.jpeg"
+      src="/assets/comics-06.jpeg"
     />
     <p>
       At this point, I was attempting to create at least one comic per day, so
       the subject matter turned to trivial things I was dealing with. Like bugs.
     </p>
     <img
-      alt="Apple Watch activity rings"
+      alt="Demon bug!!"
       className="blog-image"
-      src="/assets/comics-08.jpeg"
+      src="/assets/comics-07.jpeg"
     />
     <p>And exercise.</p>
     <img
-      alt="I will say nothing"
+      alt="Apple Watch activity rings"
       className="blog-image"
-      src="/assets/comics-09.jpeg"
+      src="/assets/comics-08.jpeg"
     />
     <p>
       There was a third panel here that turned out bad, so I cut it off. I
       enjoyed this two-panel reaction shot enough to post it despite that.
     </p>
     <img
-      alt="Flat La Croix"
+      alt="I will say nothing"
       className="blog-image"
-      src="/assets/comics-10.jpeg"
+      src="/assets/comics-09.jpeg"
     />
     <p>
       Most of my comics didn't warrant a title. Perhaps this one didn't, either,
       but I decided to include it for some additional context.
     </p>
     <img
+      alt="Flat La Croix"
+      className="blog-image"
+      src="/assets/comics-10.jpeg"
+    />
+    <p>My son and I take pictures of our silly faces sometimes.</p>
+    <img
       alt="Silly faces"
       className="blog-image"
       src="/assets/comics-11.jpeg"
     />
-    <p>My son and I take pictures of our silly faces sometimes.</p>
-    <img alt="Hair study" className="blog-image" src="/assets/comics-12.jpeg" />
     <p>
       My hairstyle was in transition during this time. I did a few quick studies
       to nail how I wanted to portray it on the page.
     </p>
-    <img alt="Duck" className="blog-image" src="/assets/comics-13.jpeg" />
+    <img alt="Hair study" className="blog-image" src="/assets/comics-12.jpeg" />
+
     <p>
       While simple, this is the first one where I played with the perspective.
       Perhaps a sequel is warranted, "honk."
     </p>
-    <img
-      alt="Butterflies"
-      className="blog-image"
-      src="/assets/comics-14.jpeg"
-    />
+    <img alt="Duck" className="blog-image" src="/assets/comics-13.jpeg" />
+
     <p>
       The goal of this study was to illustrate butterflies at various levels of
       resolution in interesting ways. Note that they become hearts as they fade
       into the distance.
     </p>
     <img
+      alt="Butterflies"
+      className="blog-image"
+      src="/assets/comics-14.jpeg"
+    />
+
+    <p>Again, hair transition issues.</p>
+    <img
       alt="Hair problems"
       className="blog-image"
       src="/assets/comics-15.jpeg"
     />
-    <p>Again, hair transition issues.</p>
+
+    <p>
+      Here, I attempted to portray a simple thing in comic form that couldn't
+      happen in reality.
+    </p>
     <img
       alt="Disappearing can"
       className="blog-image"
       src="/assets/comics-16.jpeg"
     />
+
     <p>
-      Here, I attempted to portray a simple thing in comic form that couldn't
-      happen in reality.
+      Using line thickness and texture, I created a biopic about our RoboVac.
     </p>
     <img
       alt="Robot vacuum"
@@ -172,55 +177,57 @@ export default () => (
       src="/assets/comics-17.jpeg"
     />
     <p>
-      Using line thickness and texture, I created a biopic about our RoboVac.
-    </p>
-    <img alt="RAAWRRRRR" className="blog-image" src="/assets/comics-18.jpeg" />
-    <p>
       After a brief hiatus, I came back to comics with a fresh perspective on my
       previous panels. I had become too careful, and it was time to loosen up a
       bit. This cat comic could be seen as a more feral version of the original
       "MAKE COMICS" strip.
+    </p>
+    <img alt="RAAWRRRRR" className="blog-image" src="/assets/comics-18.jpeg" />
+
+    <p>
+      These few comics were illustrated around Halloween 2018. I drew the candy
+      witch, a witch who trades a toy for candy.
     </p>
     <img
       alt="Candy witch"
       className="blog-image"
       src="/assets/comics-19.jpeg"
     />
-    <p>
-      These few comics were illustrated around Halloween 2018. I drew the candy
-      witch, a witch who trades a toy for candy.
-    </p>
+
+    <p>I also found the idea of an angsty pumpkin hilarious.</p>
     <img
       alt="Angsty pumpkin"
       className="blog-image"
       src="/assets/comics-20.jpeg"
     />
-    <p>I also found the idea of an angsty pumpkin hilarious.</p>
-    <img
-      alt="Contemplative fisherman"
-      className="blog-image"
-      src="/assets/comics-21.jpeg"
-    />
+
     <p>
       This started out as a hand study, but resulted in one of my favorite
       strips - more realistic with multiple perspectives. There is a nostalgia
       to it, as well. I haven't gone fishing in years, if not decades.
     </p>
     <img
-      alt="Dragon costume"
+      alt="Contemplative fisherman"
       className="blog-image"
-      src="/assets/comics-22.jpeg"
+      src="/assets/comics-21.jpeg"
     />
+
     <p>
       Back to the Halloween theme, I thought it would be fun to illustrate a
       dragon - with a twist. It was my son's costume all along.
     </p>
     <img
+      alt="Dragon costume"
+      className="blog-image"
+      src="/assets/comics-22.jpeg"
+    />
+
+    <p>Finally, too much caffeine!</p>
+    <img
       alt="Coffee high"
       className="blog-image"
       src="/assets/comics-23.jpeg"
     />
-    <p>Finally, too much caffeine!</p>
     <p>
       I had a lot of fun with these, and I hope to do more. My main takeaway
       from the project is that I can (and should) impose constraints upon myself
