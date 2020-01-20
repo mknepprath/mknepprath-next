75367be680880a94c1c3bf861b2cd2dc8cc4e110
m0k013z <mknepprath@gmail.com>
2020-01-20 18:47:08 -0500

    Add links

--- pages/uses.js
+++ pages/uses.js
@@ -11,45 +11,129 @@ export default () => (
 
       <h2>Editor & Terminal</h2>
       <ul>
-        <li>Visual Studio Code</li>
-        <li>Night Owl by Sarah Drasner</li>
-        <li>Dank Mono</li>
-        <li>Terminal.app with Night Owl theme</li>
+        <li>
+          <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+        </li>
+        <li>
+          <a href="https://marketplace.visualstudio.com/items?itemName=sdras.night-owl">
+            Night Owl
+          </a>{" "}
+          by <a href="https://twitter.com/sarah_edo">Sarah Drasner</a>
+        </li>
+        <li>
+          <a href="https://dank.sh/">Dank Mono</a> font by{" "}
+          <a href="https://twitter.com/_philpl">Phil Pluckthun</a>
+        </li>
+        <li>
+          Terminal.app with the{" "}
+          <a href="https://github.com/mcansh/night-owl-terminal">Night Owl</a>{" "}
+          theme (that I tweaked a bit)
+        </li>
       </ul>
 
       <h2>Desktop Apps</h2>
       <ul>
-        <li>Brave</li>
-        <li>1Password</li>
-        <li>Things (macOS & iOS)</li>
-        <li>Shotty</li>
-        <li>Vanilla</li>
-        <li>TablePlus</li>
-        <li>PDF Expert</li>
-        <li>Spotify</li>
-        <li>Sketch</li>
-        <li>Abstract</li>
-        <li>Figma</li>
-        <li>Wallcat</li>
-        <li>Aerial Screen Saver</li>
+        <li>
+          <a href="https://brave.com/">Brave</a>
+        </li>
+        <li>
+          <a href="https://1password.com/">1Password</a>
+        </li>
+        <li>
+          <a href="https://culturedcode.com/things/">Things</a> (macOS & iOS)
+        </li>
+        <li>
+          <a href="https://meetshotty.com/">Shotty</a> by{" "}
+          <a href="https://twitter.com/JacobRuizDesign">Jacob Ruiz</a>
+        </li>
+        <li>
+          <a href="https://matthewpalmer.net/vanilla/">Vanilla</a> by{" "}
+          <a href="https://twitter.com/_matthewpalmer">Matthew Palmer</a>
+        </li>
+        <li>
+          <a href="https://www.spotify.com/">Spotify</a> (
+          <a href="https://open.spotify.com/user/mknepprath">mknepprath</a>)
+        </li>
+        <li>
+          <a href="https://www.sketch.com/">Sketch</a>
+        </li>
+        <li>
+          <a href="https://www.abstract.com/">Abstract</a>
+        </li>
+        <li>
+          <a href="https://www.figma.com/">Figma</a>
+        </li>
+        <li>
+          <a href="https://tableplus.com/">TablePlus</a>
+        </li>
+        <li>
+          <a href="https://pdfexpert.com/">PDF Expert</a>
+        </li>
+        <li>
+          <a href="https://beta.wall.cat/">Wallcat</a> by{" "}
+          <a href="https://twitter.com/SmallAnimalInc">Small Animal Studios</a>
+        </li>
+        <li>
+          <a href="https://github.com/JohnCoates/Aerial">Aerial Screensaver</a>
+        </li>
       </ul>
 
       <h2>Brave/Chrome Extensions</h2>
       <p>Since Brave is Chromium, Chrome extensions work for both.</p>
       <ul>
-        <li>1Password</li>
-        <li>React Developer Tools</li>
-        <li>Instapaper</li>
-        <li>Patched Tomatoes</li>
-        <li>JSON Formatter</li>
+        <li>
+          <a href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi">
+            React Developer Tools
+          </a>
+        </li>
+        <li>
+          <a href="https://chrome.google.com/webstore/detail/instapaper/ldjkgaaoikpmhmkelcgkgacicjfbofhh">
+            Instapaper
+          </a>
+        </li>
+        <li>
+          <a href="https://mknepprath.com/patched-tomatoes">Patched Tomatoes</a>
+        </li>
+        <li>
+          <a href="https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa">
+            JSON Formatter
+          </a>
+        </li>
       </ul>
 
       <h2>Desktop IRL</h2>
-      <p>The things on my real desk that I use everyday.</p>
+      <p>
+        The things on{" "}
+        <a href="https://twitter.com/mknepprath/status/1215106277447282689?s=20">
+          my real desk
+        </a>{" "}
+        that I use everyday.
+      </p>
       <ul>
-        <li>MacBook Pro (15-inch, 2018)</li>
-        <li>Apple Thunderbolt Display</li>
-        <li>Magic Mouse</li>
+        <li>
+          <a href="https://www.apple.com/shop/buy-mac/macbook-pro">
+            MacBook Pro
+          </a>{" "}
+          (15-inch, 2018)
+        </li>
+        <li>
+          <a href="https://www.amazon.com/Apple-MC914LL-27-inch-Thunderbolt-Display/dp/B074D4W31Y">
+            Apple Thunderbolt Display
+          </a>
+        </li>
+        <li>
+          <a href="https://www.apple.com/airpods">AirPods</a>
+        </li>
+        <li>
+          <a href="https://www.apple.com/shop/product/MLA02LL/A/magic-mouse-2-silver">
+            Magic Mouse
+          </a>
+        </li>
+        <li>
+          <a href="https://www.westelm.com/products/modern-wall-desk-white-pecan-h1499/">
+            West Elm Modern Wall Desk
+          </a>
+        </li>
       </ul>
     </article>
   </Page>
