f6a928b7655643ff5a25b3f85475e2a934d89778
Michael Knepprath <mknepprath@Enoch.attlocal.net>
2020-05-03 12:27:03 -0400

    Add empty icons and 1x1 route

---
+++ pages/icon/1x1/index.js
@@ -0,0 +1,11 @@
+import NextHead from "next/head";
+
+export default () => (
+    <>
+        <NextHead>
+            <link rel="apple-touch-icon" sizes="57x57" href="/assets/icon/1x1.png"></link>
+        </NextHead>
+        
+        <img src="/assets/icon/1x1.png" />
+    </>
+)
