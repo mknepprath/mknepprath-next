eaca3627ae81ce223cfc551e9fc9606b857d7e20
Michael Knepprath <mknepprath@gmail.com>
2020-06-09 16:40:06 -0400

    Fix og:image for posts

--- core/head.js
+++ core/head.js
@@ -1,6 +1,11 @@
 import NextHead from "next/head";
 import PropTypes from "prop-types";
 
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "localhost:3000"
+    : "https://mknepprath.com";
+
 function Head({ title, description, ogImage, url }) {
   return (
     <NextHead>
@@ -29,8 +34,8 @@ function Head({ title, description, ogImage, url }) {
       <meta property="og:description" content={description} />
       <meta name="twitter:site" content={url} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={ogImage} />
-      <meta property="og:image" content={ogImage} />
+      <meta name="twitter:image" content={`${BASE_URL}${ogImage}`} />
+      <meta property="og:image" content={`${BASE_URL}${ogImage}`} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="pinterest" content="nopin" />
