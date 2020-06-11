cbddbb1a0bc9e414c0c32906e8dbf0fe25e43427
Michael Knepprath <mknepprath@gmail.com>
2020-06-11 07:38:59 -0400

    Add CI to homepage projects

--- data/links.js
+++ data/links.js
@@ -4,8 +4,8 @@ export const footerLinks = [
   {
     href: "mailto:michael@mknepprath.com",
     key: "email",
-    title: "Email Me"
-  }
+    title: "Email Me",
+  },
 ];
 
 /** @type {Array<{ href: string, key: string, title: string }>} */
@@ -13,82 +13,88 @@ export const navLinks = [
   {
     href: "https://github.com/mknepprath",
     key: "code",
-    title: "GitHub"
+    title: "GitHub",
   },
   {
     href: "https://dribbble.com/mknepprath",
     key: "illustration",
-    title: "Dribbble"
-  }
+    title: "Dribbble",
+  },
 ];
 
 /** @type {Array<{ description: string, href: string, title: string, imgSrc: string }>} */
 export const projectLinks = [
   {
+    description: "A website based on a podcast",
+    href: "https://culturallyirrelevant.net/",
+    title: "Culturally Irrelevant",
+    imgSrc: "/assets/culturally-irrelevant.png",
+  },
+  {
     description: "A Twitter text adventure",
     href: "https://twitter.com/familiarlilt",
     title: "Lilt",
-    imgSrc: "/assets/lilt.png"
+    imgSrc: "/assets/lilt.png",
   },
   {
     description: "A prompt a day, every day",
     href: "https://twitter.com/designprompts",
     title: "Design Prompts",
-    imgSrc: "/assets/design-prompts.png"
+    imgSrc: "/assets/design-prompts.png",
   },
   {
     description: "Illustration series",
     href:
       "https://dribbble.com/mknepprath/buckets/257652-Design-Prompts-Series",
     title: "Dribbble Series",
-    imgSrc: "/assets/dribbble-series.png"
+    imgSrc: "/assets/dribbble-series.png",
   },
   {
     description: "@horse_ebooks Twitter bot",
     href: "https://twitter.com/robot_mk",
     title: "Robot MK",
-    imgSrc: "/assets/robot-mk.png"
+    imgSrc: "/assets/robot-mk.png",
   },
   {
     description: "A thread of Cinema 4D renders",
     href: "https://twitter.com/mknepprath/status/959812218119512065",
-    title: "Cinema 4D Animations"
+    title: "Cinema 4D Animations",
   },
   {
     description: "Chrome extension",
     href: "/patched-tomatoes",
     title: "Patched Tomatoes",
-    imgSrc: "/assets/patched-tomatoes.png"
+    imgSrc: "/assets/patched-tomatoes.png",
   },
   {
     description: "React RPG framework",
     href: "https://github.com/mknepprath/open-world",
-    title: "Open World"
+    title: "Open World",
   },
   {
     description: "A comic retrospective",
     href: "/writing/sequential-art",
-    title: "Sequential Art™"
+    title: "Sequential Art™",
   },
   {
     description: "Battle your friends on Twitter",
     href: "https://twitter.com/bout_bot",
-    title: "Bout"
+    title: "Bout",
   },
   {
     description: "College projects",
     href: "https://vimeo.com/mknepprath",
     title: "Film & Animation",
-    imgSrc: "/assets/film.png"
+    imgSrc: "/assets/film.png",
   },
   {
     description: "A Minecraft server",
     href: "/dynoland",
-    title: "Dynoland"
+    title: "Dynoland",
   },
   {
     description: "GIF collection",
     href: "/gifs",
-    title: "GIFs"
-  }
+    title: "GIFs",
+  },
 ];
