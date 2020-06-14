b9a5b83c0a1bea79c1940479bb45e64e4ff6720c
Michael Knepprath <mknepprath@gmail.com>
2020-06-14 17:25:39 -0400

    Add zombie flash animation

---
+++ pages/lab/flash/zombies/index.js
@@ -0,0 +1,13 @@
+export default function Zombies() {
+  return (
+    <object
+      type="application/x-shockwave-flash"
+      data="/images/zombies.swf"
+      width="800"
+      height="600"
+    >
+      <param name="movie" value="/images/zombies.swf" />
+      <param name="quality" value="high" />
+    </object>
+  );
+}
