e7549787dc33794d0a46bfd4e1fcb24d57f3c2f4
Michael Knepprath <mknepprath@gmail.com>
2020-06-23 09:13:22 -0400

    Add post, link to brush page, query parameters

---
+++ core/pxbrush.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const BRUSH_LENGTH = 320;
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+interface Props {
+  coordinates: Array<number>;
+  increment: number;
+}
+
+export default function PxBrushPage(props: Props) {
+  const [x, y] = props.coordinates;
+
+  const [nodes, setNodes] = React.useState([
+    { color: "rgba(51, 51, 51, 1)", id: `${x},${y}`, x, y },
+  ]);
+
+  React.useEffect(() => {
+    let id = setInterval(() => {
+      setNodes((prevNodes) => {
+        let nextX =
+          prevNodes[prevNodes.length - 1].x +
+          [-props.increment, 0, props.increment][getRandomInt(3)];
+        let nextY =
+          prevNodes[prevNodes.length - 1].y +
+          [-props.increment, 0, props.increment][getRandomInt(3)];
+        const nextNodes = [
+          ...prevNodes,
+          {
+            color: [
+              "rgba(120, 159, 177, .4)",
+              "rgba(99, 91, 137, .4)",
+              "rgba(226, 135, 164, .4)",
+              "rgba(255, 207, 186, .4)",
+              "rgba(51, 51, 51, 1)",
+            ][getRandomInt(5)],
+            id: `${nextX},${nextY}`,
+            x: nextX < 0 ? 0 : nextX,
+            y: nextY < 0 ? 0 : nextY,
+          },
+        ];
+        if (nextNodes.length > BRUSH_LENGTH) nextNodes.shift();
+        return nextNodes;
+      });
+    }, 1);
+    return () => clearInterval(id);
+  });
+
+  return (
+    <>
+      {nodes.map(({ x, y, color }, index) => (
+        <div
+          key={index}
+          style={{
+            backgroundColor: color,
+            height: props.increment,
+            left: x,
+            pointerEvents: "none",
+            position: "absolute",
+            width: props.increment,
+            top: y,
+          }}
+        />
+      ))}
+    </>
+  );
+}
--- pages/writing/life.tsx
+++ pages/writing/life.tsx
@@ -1,91 +1,56 @@
 import React, { useEffect } from "react";
+import Link from "next/link";
 
 import BlogPage from "core/blog-page";
-
-import styles from "./life.module.css";
-
-const INC = 6;
-
-function getRandomInt(max: number) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+import PxBrush from "core/pxbrush";
 
 export const meta = {
-  // image: "/assets/twitter-likes-illustration-1.jpg",
-  // published: true,
-  publishedAt: "2020-06-21",
-  summary: "Life, the universe, and everything.",
-  title: "The Game of Life",
+  image: "/assets/pxbrush.png",
+  published: true,
+  publishedAt: "2020-06-23",
+  summary: "Recreating a project from my high school years.",
+  title: "Life, Art, Nostalgia",
 };
 
 export default function Life() {
-  const [nodes, setNodes] = React.useState([
-    { color: "rgba(51, 51, 51, 1)", id: `${0},${0}`, x: 0, y: 0 },
-  ]);
+  const [centerpoint, setCenterpoint] = React.useState<Array<number>>();
+
+  const brushEl = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    let id = setInterval(() => {
-      setNodes((prevNodes) => {
-        let nextX =
-          prevNodes[prevNodes.length - 1].x + [-INC, 0, INC][getRandomInt(3)];
-        let nextY =
-          prevNodes[prevNodes.length - 1].y + [-INC, 0, INC][getRandomInt(3)];
-        const nextNodes = [
-          ...prevNodes,
-          {
-            color: [
-              "rgba(120, 159, 177, .4)",
-              "rgba(99, 91, 137, .4)",
-              "rgba(226, 135, 164, .4)",
-              "rgba(255, 207, 186, .4)",
-              "rgba(51, 51, 51, 1)",
-            ][getRandomInt(5)],
-            id: `${nextX},${nextY}`,
-            x: nextX < 0 ? 0 : nextX,
-            y: nextY < 0 ? 0 : nextY,
-          },
-        ];
-        if (nextNodes.length > 100) nextNodes.shift();
-        return nextNodes;
-      });
-    }, 5);
-    return () => clearInterval(id);
-  });
+    if (brushEl.current) {
+      var rect = brushEl.current.getBoundingClientRect();
+
+      const centerX = rect.left + (rect.right - rect.left) / 2;
+      const centerY = rect.top + (rect.bottom - rect.top) / 2;
+
+      const xToScaleGrid = centerX - (centerX % 16);
+      const yToScaleGrid = centerY - (centerY % 16);
+
+      setCenterpoint(() => [xToScaleGrid, yToScaleGrid]);
+    }
+  }, []);
 
   return (
     <BlogPage
       dateTime={meta.publishedAt}
       description={meta.summary}
       highlightCode
-      // ogImage={meta.image}
+      ogImage={meta.image}
       title={meta.title}
     >
-      {/* <img
-        alt="Likes header illustration"
-        className="blog-image"
-        src="/assets/twitter-likes-illustration-1.jpg"
-      /> */}
+      <div ref={brushEl} style={{ height: 300, width: "100%" }} />
+      {centerpoint && <PxBrush coordinates={centerpoint} increment={16} />}
+
       <header>
         <h1>{meta.title}</h1>
       </header>
-      <p>
-        I highly recommend you do NOT leave this page open. There are currently{" "}
-        {nodes.length} nodes on the page...
-      </p>
-      {nodes.map(({ x, y, color }, index) => (
-        <div
-          className={styles.px}
-          key={index}
-          style={{
-            // borderRadius: INC / 2,
-            backgroundColor: color,
-            height: INC,
-            left: x,
-            width: INC,
-            top: y,
-          }}
-        />
-      ))}
+      <p>I highly recommend you do NOT leave this page open.</p>
+      <Link href="/pxbrush?scale=30&quantity=1">
+        <a>Scale 30</a>
+      </Link>
     </BlogPage>
   );
 }
+
+// /pxbrush?scale=6&quantity=10
