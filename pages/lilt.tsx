9c82ab8f40c60366da0c5b8f263b3d11b93db614
Michael Knepprath <mknepprath@gmail.com>
2020-08-05 08:13:36 -0400

    Update Lilt page

--- pages/lilt.tsx
+++ pages/lilt.tsx
@@ -10,7 +10,14 @@ const Lilt = () => {
 
         <img alt="Lilt logo" src="/assets/lilt.png" />
 
-        <p>Every game should have a guidebook. Here's Lilt's.</p>
+        <p>Every game should have a guidebook. Here's <a href="https://twitter.com/familiarlilt" rel="noopener noreferrer" target="_blank">Lilt</a>'s.</p>
+
+        <h2>The Goal</h2>
+        <p>You find yourself trapped in a mysterious room. How do you get out?</p>
+
+        <h2>Tips</h2>
+        <p>While Lilt attempts to allow you to use natural language messages, it works best if you follow certain patterns: "look at the tree", "pick up the coin", "use water on the flower", and so on.</p>
+        <p>It's also possible to give items to other people who are playing the game: "Give @frederick an apple."</p>
       </article>
     </Page>
   );
