04a183926dd30cfef818a5ecac1d9512116e715e
Michael Knepprath <mknepprath@gmail.com>
2020-10-24 22:26:12 -0400

    Add evolvesFrom

--- pages/api/v1/released-pokemon.ts
+++ pages/api/v1/released-pokemon.ts
@@ -32,35 +32,56 @@ interface HydratedPokemon extends Pokemon {
     shinyReleased: boolean;
 }
 
+interface PokemonEvolutionBase {
+    form: string;
+    pokemon_id: number;
+    pokemon_name: string;
+}
+
+interface Evolution extends PokemonEvolutionBase {
+    candy_required: number;
+}
+
+interface PokemonEvolution extends PokemonEvolutionBase {
+    evolutions: Evolution[];
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const releasedPokemonDict: { [key: string]: Pokemon } = await fetch("https://pogoapi.net/api/v1/released_pokemon.json")
         .then((response) => response.json())
+    const releasedPokemon = Object.values(releasedPokemonDict)
+
     const nestingPokemonDict: { [key: string]: Pokemon } = await fetch("https://pogoapi.net/api/v1/nesting_pokemon.json")
         .then((response) => response.json())
+
     const raidBossesDict: {
         current: { [key: string]: [RaidBoss] }
     } = await fetch("https://pogoapi.net/api/v1/raid_bosses.json")
         .then((response) => response.json())
+    const raidBosses = Object.keys(raidBossesDict.current).flatMap(tier => raidBossesDict.current[tier])
+
     const shadowPokemonDict: { [key: string]: Pokemon } = await fetch("https://pogoapi.net/api/v1/shadow_pokemon.json")
         .then((response) => response.json())
+
     const shinyPokemonDict: { [key: string]: ShinyPokemon } = await fetch("https://pogoapi.net/api/v1/shiny_pokemon.json")
         .then((response) => response.json())
+
     const possibleDittoDict: { [key: string]: Pokemon } = await fetch("https://pogoapi.net/api/v1/possible_ditto_pokemon.json")
         .then((response) => response.json())
 
-    const releasedPokemon = Object.values(releasedPokemonDict)
-
-    const raidBosses = Object.keys(raidBossesDict.current).flatMap(tier => raidBossesDict.current[tier])
+    const evolutions: PokemonEvolution[] = await fetch("https://pogoapi.net/api/v1/pokemon_evolutions.json")
+        .then((response) => response.json())
 
     const hydratedPokemon: HydratedPokemon[] = releasedPokemon.map(
         p => ({
             ...p,
+            evolvesFrom: evolutions.find(e => e.evolutions.find(e => e.pokemon_id === p.id))?.pokemon_id,
             nests: Boolean(nestingPokemonDict[p.id]),
+            possibleDitto: Boolean(possibleDittoDict[p.id]),
             raidBoss: Boolean(raidBosses.find(boss => boss.id === p.id)),
             raidBossTier: String(raidBosses.find(boss => boss.id === p.id)?.tier || 0),
             shadowObtainable: Boolean(shadowPokemonDict[p.id]),
-            shinyReleased: Boolean(shinyPokemonDict[p.id]),
-            possibleDitto: Boolean(possibleDittoDict[p.id])
+            shinyReleased: Boolean(shinyPokemonDict[p.id])
         })
     )
 
