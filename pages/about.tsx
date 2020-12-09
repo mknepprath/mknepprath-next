3752e54d1743200ca2773df89184e38750296423
Michael Knepprath <mknepprath@gmail.com>
2020-12-09 08:26:09 -0500

    Add books and films to About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,20 @@
 import classnames from "classnames";
+import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
 
+import Card from "core/card";
 import Page from "core/page";
 import PhotoStack from "core/photo-stack";
 
 import styles from "./about.module.css";
 
+const fetcher = (url: RequestInfo) =>
+  fetch(url).then((response) => response.json());
+
 export default function About() {
+  const { data: books } = useSWR(`/api/v1/books`, fetcher);
+  const { data: films } = useSWR(`/api/v1/films`, fetcher);
+
   return (
     <Page
       className={classnames("container", styles.pageContainer)}
@@ -46,6 +55,40 @@ export default function About() {
           </a>
           .
         </p>
+
+        {books?.length ? (
+          <>
+            <h2>Books I've Read</h2>
+            <div className={styles.bookContainer}>
+              {books?.map((book: Book) => (
+                <Card
+                  description={book.author}
+                  href={book.link}
+                  imgSrc={book.image_url}
+                  key={book.link}
+                  title={book.title}
+                />
+              ))}
+            </div>
+          </>
+        ) : null}
+
+        {films?.length ? (
+          <>
+            <h2>Films I've Watched</h2>
+            <div className={styles.bookContainer}>
+              {films?.map((film: Film) => (
+                <Card
+                  description={film.year}
+                  href={film.link}
+                  imgSrc={film.image_url.split('"')[1]}
+                  key={film.link}
+                  title={film.title}
+                />
+              ))}
+            </div>
+          </>
+        ) : null}
       </article>
     </Page>
   );
---
+++ pages/api/v1/films.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fetch from "isomorphic-unfetch";
+import xml2js from "xml2js";
+
+const LETTERBOXD_RSS = `https://letterboxd.com/mknepprath/rss/`;
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let films: Film[] = [];
+  await fetch(LETTERBOXD_RSS)
+    .then((response) => response.text())
+    .then((body) => {
+      xml2js.parseString(body, function (error, response) {
+        if (error) console.error(error);
+        console.info("Getting Film List from Letterboxd API");
+        let filmList = response.rss.channel[0].item;
+        for (let i = 0; i < filmList.length; i++) {
+          films.push({
+            title: filmList[i]["letterboxd:filmTitle"][0],
+            year: filmList[i]["letterboxd:filmYear"][0],
+            image_url: filmList[i].description[0],
+            link: filmList[i].link[0],
+            rating: filmList[i]["letterboxd:memberRating"]?.[0],
+          });
+        }
+      });
+    })
+    .catch((error) => console.error(error));
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "max-age=86400");
+  res.end(JSON.stringify(films.filter((film) => +film.rating > 3).slice(0, 6)));
+};
