f6b2aca3413b93cf8bc68fd18f6e12a2ae54a449
Michael Knepprath <mknepprath@gmail.com>
2021-01-23 10:12:35 -0500

    Add reply count + icons

--- core/blog-page.tsx
+++ core/blog-page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import classnames from "classnames";
-import format from "date-fns/format";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import parseISO from "date-fns/parseISO";
 import Prism from "prismjs";
@@ -10,6 +9,7 @@ import A from "core/a";
 import BlogNav from "core/blog-nav";
 import Footer from "core/footer";
 import Head from "core/head";
+import RepliesIcon from "core/icon-replies";
 import Nav from "core/nav";
 
 const fetcher = (url: RequestInfo): Promise<Tweet> =>
@@ -64,15 +64,16 @@ export default function BlogPage({
         {children}
         <p className="blog-meta">
           <time dateTime={dateTime}>
-            {format(parseISO(dateTime), "MMMM d, yyyy")} •{" "}
-            {formatDistanceToNow(parseISO(dateTime))} ago
+            Published {formatDistanceToNow(parseISO(dateTime))} ago
           </time>
           {tweet?.data ? (
             <>
-              {" "}
-              •{" "}
               <A href={`https://twitter.com/mknepprath/status/${tweetId}`}>
-                {tweet?.data?.public_metrics.like_count} likes
+                ♥ {tweet?.data?.public_metrics.like_count} likes
+              </A>
+              <A href={`https://twitter.com/mknepprath/status/${tweetId}`}>
+                <RepliesIcon /> {tweet?.data?.public_metrics.reply_count}{" "}
+                replies
               </A>
             </>
           ) : (
@@ -81,7 +82,7 @@ export default function BlogPage({
         </p>
       </article>
 
-      <BlogNav />
+      {/* <BlogNav /> */}
       <Footer />
     </div>
   );
---
+++ core/icon-replies.tsx
@@ -0,0 +1,12 @@
+import styles from "./icon-replies.module.css";
+
+export default function RepliesIcon(): JSX.Element {
+  return (
+    <svg className={styles.svg} viewBox="0 0 100 97">
+      <path
+        fill="#6ABD9D"
+        d="M35 4C15.67 4 0 19.67 0 39C0 57.9955 15.1325 73.4567 34 73.986V96.3364C34 99.5688 37.6353 101.466 40.2872 99.618L85.0127 68.4457C94.4032 61.9008 100 51.1779 100 39.7317V39C100 19.67 84.33 4 65 4H35Z"
+      />
+    </svg>
+  );
+}
