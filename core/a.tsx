19c9e3e31320dac262940162aef319000e647a4d
Michael Knepprath <mknepprath@gmail.com>
2021-03-19 21:49:04 -0400

    Update /uses

--- core/a.tsx
+++ core/a.tsx
@@ -1,18 +1,18 @@
 interface Props {
-  children: React.ReactChild | React.ReactChild[];
+  children: React.ReactNode;
   className?: string;
   href: string;
 }
 
-export default function A(props: Props): JSX.Element {
-  return (
-    <a
-      className={props.className}
-      href={props.href}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      {props.children}
-    </a>
-  );
-}
+const A = (props: Readonly<Props>): React.ReactElement => (
+  <a
+    className={props.className}
+    href={props.href}
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    {props.children}
+  </a>
+);
+
+export default A;
--- pages/uses.tsx
+++ pages/uses.tsx
@@ -1,3 +1,4 @@
+import A from "core/a";
 import Page from "core/page";
 
 export default function Uses(): React.ReactNode {
@@ -11,27 +12,27 @@ export default function Uses(): React.ReactNode {
         <p>
           This is where I document tools and products I use and love. If
           you&apos;re curious about anything, feel free to{" "}
-          <a href="https://twitter.com/mknepprath">@ me</a>.
+          <A href="https://twitter.com/mknepprath">@ me</A>.
         </p>
 
         <h2>Editor & Terminal</h2>
         <ul>
           <li>
-            <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+            <A href="https://code.visualstudio.com/">Visual Studio Code</A>
           </li>
           <li>
-            <a href="https://marketplace.visualstudio.com/items?itemName=sdras.night-owl">
+            <A href="https://marketplace.visualstudio.com/items?itemName=sdras.night-owl">
               Night Owl
-            </a>{" "}
-            by <a href="https://twitter.com/sarah_edo">Sarah Drasner</a>
+            </A>{" "}
+            by <A href="https://twitter.com/sarah_edo">Sarah Drasner</A>
           </li>
           <li>
-            <a href="https://dank.sh/">Dank Mono</a> font by{" "}
-            <a href="https://twitter.com/_philpl">Phil Pluckthun</a>
+            <A href="https://dank.sh/">Dank Mono</A> font by{" "}
+            <A href="https://twitter.com/_philpl">Phil Pluckthun</A>
           </li>
           <li>
             Terminal.app with the{" "}
-            <a href="https://github.com/mcansh/night-owl-terminal">Night Owl</a>{" "}
+            <A href="https://github.com/mcansh/night-owl-terminal">Night Owl</A>{" "}
             theme (that I tweaked a bit)
           </li>
         </ul>
@@ -39,188 +40,130 @@ export default function Uses(): React.ReactNode {
         <h2>Apps</h2>
         <ul>
           <li>
-            <a href="https://www.microsoft.com/en-us/edge">Microsoft Edge</a>
+            <A href="https://www.microsoft.com/en-us/edge">Microsoft Edge</A>
           </li>
           <li>
-            <a href="https://1password.com/">1Password</a>
+            <A href="https://1password.com/">1Password</A>
           </li>
           <li>
-            <a href="https://culturedcode.com/things/">Things</a>
+            <A href="https://culturedcode.com/things/">Things</A>
           </li>
           <li>
-            <a href="https://flexibits.com/fantastical">Fantastical</a> by{" "}
-            <a href="https://twitter.com/flexibits">Flexibits</a>
+            <A href="https://flexibits.com/fantastical">Fantastical</A> by{" "}
+            <A href="https://twitter.com/flexibits">Flexibits</A>
           </li>
           <li>
-            <a href="https://slack.com/">Slack</a>
+            <A href="https://slack.com/">Slack</A>
           </li>
           <li>
-            <a href="http://notion.so/">Notion</a>
+            <A href="https://reincubate.com/camo/">Camo</A> by Reincubate
           </li>
           <li>
-            <a href="https://meetshotty.com/">Shotty</a> by{" "}
-            <a href="https://twitter.com/JacobRuizDesign">Jacob Ruiz</a>
+            <A href="http://notion.so/">Notion</A>
           </li>
           <li>
-            <a href="https://matthewpalmer.net/vanilla/">Vanilla</a> by{" "}
-            <a href="https://twitter.com/_matthewpalmer">Matthew Palmer</a>
+            <A href="https://meetshotty.com/">Shotty</A> by{" "}
+            <A href="https://twitter.com/JacobRuizDesign">Jacob Ruiz</A>
           </li>
           <li>
-            <a href="https://www.spotify.com/">Spotify</a> (
-            <a href="https://open.spotify.com/user/mknepprath">follow me</a>)
+            <A href="https://music.apple.com/">Apple Music</A> (
+            <A href="https://music.apple.com/profile/mknepprath">follow me</A>)
           </li>
           <li>
-            <a href="https://www.sketch.com/">Sketch</a>
+            <A href="https://www.figma.com/">Figma</A>
           </li>
           <li>
-            <a href="https://www.abstract.com/">Abstract</a>
+            <A href="https://tableplus.com/">TablePlus</A>
           </li>
           <li>
-            <a href="https://www.figma.com/">Figma</a>
+            <A href="https://pdfexpert.com/">PDF Expert</A>
           </li>
           <li>
-            <a href="https://tableplus.com/">TablePlus</a>
-          </li>
-          <li>
-            <a href="https://pdfexpert.com/">PDF Expert</a>
-          </li>
-          <li>
-            <a href="https://beta.wall.cat/">Wallcat</a> by{" "}
-            <a href="https://twitter.com/SmallAnimalInc">
+            <A href="https://beta.wall.cat/">Wallcat</A> by{" "}
+            <A href="https://twitter.com/SmallAnimalInc">
               Small Animal Studios
-            </a>
+            </A>
           </li>
           <li>
-            <a href="https://github.com/JohnCoates/Aerial">
+            <A href="https://github.com/JohnCoates/Aerial">
               Aerial Screensaver
-            </a>
+            </A>
           </li>
         </ul>
 
-        <h2>Chrome Extensions</h2>
-        <p>
-          Since Microsoft Edge is based on Chromium, Chrome extensions work for
-          both browsers.
-        </p>
+        <h2>Edge Extensions</h2>
         <ul>
           <li>
-            <a
-              href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi">
               React Developer Tools
-            </a>
+            </A>
           </li>
           <li>
-            <a
-              href="https://chrome.google.com/webstore/detail/instapaper/ldjkgaaoikpmhmkelcgkgacicjfbofhh"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://chrome.google.com/webstore/detail/instapaper/ldjkgaaoikpmhmkelcgkgacicjfbofhh">
               Instapaper
-            </a>
+            </A>
           </li>
           <li>
-            <a
-              href="https://mknepprath.com/patched-tomatoes"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Patched Tomatoes
-            </a>{" "}
-            by me
+            <A href="https://grammarly.com">Grammarly</A>
           </li>
           <li>
-            <a
-              href="https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa">
               JSON Formatter
-            </a>
+            </A>
           </li>
         </ul>
 
         <h2>Desktop IRL</h2>
         <p>
           The things on{" "}
-          <a
-            href="https://twitter.com/mknepprath/status/1215106277447282689?s=20"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
+          <A href="https://twitter.com/mknepprath/status/1215106277447282689?s=20">
             my real desk
-          </a>{" "}
+          </A>{" "}
           that I use everyday.
         </p>
         <ul>
           <li>
-            <a
-              href="https://www.westelm.com/products/modern-wall-desk-white-pecan-h1499/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://www.westelm.com/products/modern-wall-desk-white-pecan-h1499/">
               West Elm Modern Wall Desk
-            </a>
+            </A>
           </li>
           <li>
-            <a
-              href="https://www.apple.com/shop/buy-mac/macbook-pro"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://www.apple.com/shop/buy-mac/macbook-pro">
               MacBook Pro
-            </a>{" "}
-            (15-inch, 2018)
+            </A>{" "}
+            (16-inch, 2019) for work
           </li>
           <li>
-            <a
-              href="https://www.amazon.com/Apple-MC914LL-27-inch-Thunderbolt-Display/dp/B074D4W31Y"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://www.amazon.com/Apple-MC914LL-27-inch-Thunderbolt-Display/dp/B074D4W31Y">
               Thunderbolt Display
-            </a>
+            </A>
           </li>
           <li>
-            <a
-              href="https://www.apple.com/airpods"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              AirPods
-            </a>
+            <A href="https://www.apple.com/shop/buy-mac/macbook-pro">
+              MacBook Air
+            </A>{" "}
+            (13-inch, 2020)
           </li>
           <li>
-            <a
-              href="https://www.apple.com/shop/product/MLA02LL/A/magic-mouse-2-silver"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://www.apple.com/airpods">AirPods</A>
+          </li>
+          <li>
+            <A href="https://www.apple.com/shop/product/MLA02LL/A/magic-mouse-2-silver">
               Magic Mouse
-            </a>
+            </A>
           </li>
           <li>
             Rain Design&apos;s{" "}
-            <a
-              href="https://www.amazon.com/Rain-Design-mStand-Laptop-Patented/dp/B000OOYECC"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <A href="https://www.amazon.com/Rain-Design-mStand-Laptop-Patented/dp/B000OOYECC">
               mStand Laptop Stand
-            </a>
+            </A>
           </li>
         </ul>
 
         <hr />
 
         <em>
-          Featured on{" "}
-          <a href="https://uses.tech" rel="noopener noreferrer" target="_blank">
-            uses.tech
-          </a>
-          .
+          Featured on <A href="https://uses.tech">uses.tech</A>.
         </em>
       </article>
     </Page>
