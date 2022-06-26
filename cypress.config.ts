cf0e27f13e52df749727b8ce459d38313765475d
Michael Knepprath <mknepprath@gmail.com>
2022-06-26 18:19:19 -0400

    Run migration

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  viewportWidth: 1300,
+  defaultCommandTimeout: 20000,
+  video: false,
+  projectId: '2tv5un',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
