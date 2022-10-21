568c3095722fd69e223750e19321e2aee928ebc5
Michael Knepprath <mknepprath@gmail.com>
2022-10-21 07:45:25 -0400

    Use Sentry secret

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,16 +2,11 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const { SENTRY_DSN } = process.env;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://6cf25a0262e44148821a14717c0057e7@o4504019047940096.ingest.sentry.io/4504019049775104',
-  // Adjust this value in production, or use tracesSampler for greater control
+  dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
