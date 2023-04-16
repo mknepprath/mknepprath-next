c2118695c5e4206f01a72ded037ff9940ce337df
Michael Knepprath <mknepprath@gmail.com>
2023-04-16 09:21:34 -0400

    Added an age calculator.

---
+++ pages/age.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface Props {
+  year: number;
+  name?: string;
+}
+
+export default function Age({ year, name }: Props): JSX.Element {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>
+        {name || `This person`} turns {age} in {currentYear}.
+      </h1>
+    </div>
+  );
+}
+
+export async function getServerSideProps({
+  query,
+}: {
+  query: { year: string; name?: string };
+}): Promise<{ props: Props }> {
+  const props: Props = {
+    year: parseInt(query.year, 10),
+  };
+  if (query.name) props.name = query.name;
+  return {
+    props,
+  };
+}
