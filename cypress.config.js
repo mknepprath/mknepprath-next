a2ec5be8bb41bdc1144b39b37c03705736bda804
Michael Knepprath <mknepprath@gmail.com>
2025-04-21 23:02:54 -0400

    Bumped tests.

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "cypress";
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { defineConfig } = require("cypress");
+const setupPlugins = require("./cypress/plugins/index.js");
 
-export default defineConfig({
+module.exports = defineConfig({
   viewportWidth: 1300,
   defaultCommandTimeout: 20000,
   video: false,
   projectId: "2tv5un",
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      return setupPlugins(on, config);
     },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
