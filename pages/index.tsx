cab513a9990980132e9edcfca36cb74c0a4da36b
Michael Knepprath <mknepprath@gmail.com>
2026-03-28 10:21:46 -0400

    Remove 'see more' text from homepage photography section

--- pages/index.tsx
+++ pages/index.tsx
@@ -170,7 +170,6 @@ export default function Home({ initialActivity, initialShots }: Props): React.Re
                   </div>
                 );
               })}
-              <span className={styles.photoSeeMore}>see more &rarr;</span>
             </Link>
           </div>
         )}
